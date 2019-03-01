90b7f6988a5d33e89095585d647fb14a286aba83
Kristian Darlington <kristiandarlington@w134-87-168-196.wireless.uvic.ca>
2019-03-01 11:11:56 -0800

    finished board updates. initiated move.py file.

--- app/main.py
+++ app/main.py
@@ -4,10 +4,13 @@ import random
 import bottle
 
 from api import ping_response, start_response, move_response, end_response
+from board import construct_board, update_board, deconstruct_board
+from move import calculate_move
 
 @bottle.route('/')
 def index():
     return bottle.static_file('index.html', root='../static/')
+
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -22,15 +25,20 @@ def ping():
 @bottle.post('/start')
 def start():
     game_state = bottle.request.json
-
-    color = "#00FF00"
-
-    return start_response(color)
+    construct_board(game_state)
+    snake_colour = "#00FF00"
+    return start_response(snake_colour)
 
 
 @bottle.post('/move')
 def move():
     game_state = bottle.request.json
+
+    new_board = update_board(game_state)
+    my_head = game_state['you']['body'][0]
+
+    turn = game_state['turn']  # for testing
+    direction = calculate_move(new_board, my_head, turn)
 
     return move_response(direction)
 
@@ -38,7 +46,7 @@ def move():
 @bottle.post('/end')
 def end():
     game_state = bottle.request.json
-
+    deconstruct_board(game_state)
     return end_response()
 
 # Expose WSGI app (so gunicorn can find it)
