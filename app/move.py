7d65eedf856c0baf442ad37aafda2d43b8f6783c
Kristian Darlington <kristiandarlington@Kristians-MacBook-Pro.local>
2019-03-02 12:42:49 -0800

    removed availability weight in bfs

--- app/move.py
+++ app/move.py
@@ -78,8 +78,7 @@ def weighted_bfs(board, pos, health):
     food_distance = []
 
     space_weight = 0
-    # while q:
-    while q and depth <= health:
+    while q:
         current = q.pop(0)
         moves = possible_moves(board, current)
         move_coords = [get_direction_map[move](current) for move in moves]
@@ -102,7 +101,7 @@ def weighted_bfs(board, pos, health):
     food_weight = np.sum([1.0 / (np.exp(distance)) for distance in food_distance])
 
     available_food = len(food_distance)
-    return (0.03 * health * space_weight) + ((6 / (available_food + 1)) * (80.0 / (health + 1)) * food_weight),\
+    return (0.03 * health * space_weight) + ((100.0 / (health + 1)) * food_weight),\
         available_food
 
 
@@ -119,7 +118,7 @@ def get_weight(board, direction, current_head, tail, health, size):
         working_board[tail[1], tail[0]] = UNOCCUPIED
     working_board[potential_head[1], potential_head[0]] = OCCUPIED
     (move_weight, available_food) = weighted_bfs(working_board, potential_head, health)
-    move_weight = move_weight + ((8 / (available_food + 1)) * (100.0 / (health + 1))) if is_food else move_weight
+    move_weight = move_weight + ((8 / (available_food + 1)) * (80.0 / (health + 1))) if is_food else move_weight
     return move_weight
 
 
